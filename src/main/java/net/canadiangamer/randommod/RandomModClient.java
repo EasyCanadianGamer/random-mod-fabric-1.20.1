7eca653c1de038491d21f2b373249d473fcbbfc5
CanadianGamer <easygamer2019@gmail.com>
2023-07-13 15:08:57 -0400

    Fixed: - texture of the sapling Added: - Recipes for the blocks and ingot

--- src/main/java/net/canadiangamer/randommod/RandomModClient.java
+++ src/main/java/net/canadiangamer/randommod/RandomModClient.java
@@ -1,10 +1,15 @@
 package net.canadiangamer.randommod;
 
+import net.canadiangamer.randommod.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class RandomModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RANDOM_LEAVES, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RANDOM_SAPLING, RenderLayer.getCutout());
 
     }
 }
