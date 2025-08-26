58644acef497916912785934ea49a81528856d46
EasyCanadianGamer <eyadi2005@gmail.com>
2025-08-26 18:45:54 -0400

    updated to 1.21.1

---
+++ src/main/java/net/canadiangamer/RandomModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.canadiangamer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RandomModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
