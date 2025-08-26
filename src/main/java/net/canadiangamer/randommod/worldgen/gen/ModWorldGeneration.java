c8492a55b2aadfeb95fa66fbe227d42df55e8e72
EasyCanadianGamer <eyadi2005@gmail.com>
2025-08-26 18:41:42 -0400

    updated to 1.21.1

--- src/main/java/net/canadiangamer/randommod/worldgen/gen/ModWorldGeneration.java
+++
@@ -1,9 +0,0 @@
-package net.canadiangamer.randommod.worldgen.gen;
-
-public class ModWorldGeneration {
-
-    public static void generateModWorldGen() {
-        ModOreGeneration.generateOres();
-
-    }
-}
